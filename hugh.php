1c745437322aa9a492ce0f65309e7a12ba90f242
George Stephanis <george@stephanis.info>
2016-02-01 21:50:40 -0600

    Declare hugh.php

---
+++ hugh.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name: Hugh
+ * Plugin URI:  https://wordpress.org/plugins/hugh/
+ * Description: Democratize coloring.
+ * Version:     0.1.0
+ * Author:      Michael Arestad and George Stephanis
+ * Author URI:  http://blog.michaelarestad.com
+ * Text Domain: hugh
+ * Domain Path: /languages
+ */
+
