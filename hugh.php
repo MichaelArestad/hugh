93647487a34f6206c4ae3253a987688a1bf4a4e0
George Stephanis <george@stephanis.info>
2016-02-01 21:57:35 -0600

    Make a widget and instantiate it.

--- hugh.php
+++ hugh.php
@@ -11,3 +11,44 @@
  * Domain Path: /languages
  */
 
+class Hugh { // Hugh is classy as fuck.
+
+	public static function add_hooks() {
+		add_action( 'widgets_init', array( __CLASS__, 'widgets_init' ) );
+	}
+
+	public static function widgets_init() {
+		register_widget( 'Hugh_Widget' );
+	}
+}
+
+Hugh::add_hooks();
+
+class Hugh_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'class_name'  => 'hugh_widget',
+			'description' => __( 'Hugh is classy.', 'hugh' ),
+		);
+		parent::__construct( 'hugh_widget', 'Hugh Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		?>
+		<h1>STUFF HAPPENS HERE</h1>
+		<?php
+		echo $args['after_widget'];
+	}
+
+}
