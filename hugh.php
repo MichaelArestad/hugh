cf347685b0b67a28aae6a9a9f3f89568996e71c6
George Stephanis <george@stephanis.info>
2016-02-04 22:40:16 -0600

    Update version number.

--- hugh.php
+++ hugh.php
@@ -4,7 +4,7 @@
  * Plugin Name: Hugh
  * Plugin URI:  https://wordpress.org/plugins/hugh/
  * Description: Democratize coloring.
- * Version:     0.1.0
+ * Version:     1.0
  * Author:      Michael Arestad and George Stephanis
  * Author URI:  http://blog.michaelarestad.com
  * Text Domain: hugh
